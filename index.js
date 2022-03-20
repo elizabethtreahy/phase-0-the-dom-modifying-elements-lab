f78ea3f555f83a683044551cd9435a29881b68c8
Elizabeth <elizabethtreahy@gmail.com>
2022-03-20 13:17:18 -0400

    commit tree 308b2e256b9732d52348e2a6e19b58553778525a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const body = document.getElementById(main)
+main.remove(main)
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "<h1>Elizabeth is the champion</h1>"
+document.body.append(newHeader)
+
+// const newHeader2 = document.createElement("h2")
+// newHeader2.id = "Elizabeth is the champion"
+// // getElementById("newHeader")
+// newHeader.innerHTML = "<h2></h2>"
